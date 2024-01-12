586f8635dd4794e65ae2da9cf99e9facc216286f
juligarc91 <julissagarcia@pursuit.org>
2024-01-12 01:00:00 -0500

    added more background images

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,10 @@ function Root() {
         'https://www.bbg.org/img/uploads/lightbox/_lightbox_retina/starburst_MS_51860951755.jpg',
         'https://www.bbg.org/img/uploads/lightbox/_lightbox_retina/snakes-head-fritillary_MS_52020358788.jpg',
         'https://www.bbg.org/img/uploads/lightbox/_lightbox_retina/spanish-bluebell_SS.jpg',
-        'https://www.bbg.org/img/uploads/lightbox/_lightbox_retina/wildcolumbine_BB_26114708833.jpg'
+        'https://www.bbg.org/img/uploads/lightbox/_lightbox_retina/wildcolumbine_BB_26114708833.jpg',
+        'https://www.bbg.org/img/uploads/lightbox/_lightbox_retina/early-flowering-cherry_MS_BBG3933.jpg',
+        'https://www.bbg.org/img/uploads/lightbox/_lightbox_retina/15768824452_maple.jpg',
+        'https://www.bbg.org/img/uploads/lightbox/_lightbox_retina/blackberry-lily_MS_50397559346_1.jpg'
     ];
 
     const [backgroundImage, setBackgroundImage] = useState(initialBackgroundImage);
