df5d6e3dab0a454f447d38a05b79fee988344178
juligarc91 <julissagarcia@pursuit.org>
2024-01-11 21:35:46 -0500

    created ToDoList props for ToDoItem

---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,20 @@
+const ToDoItem = ({ completed, id, title }) => {
+  return (
+    <li>
+        <label>
+            <input 
+                type="checkbox" 
+                checked={completed} 
+                onChange={e => toggleTodo(id, e.target.checked)}
+            />
+            {title}
+        </label>
+            <button 
+                onClick={()=>deleteTodo(id)} 
+                className="btn btn-danger"
+            >Delete</button>
+    </li>
+  )
+}
+
+export default ToDoItem
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,21 +1,29 @@
+import ToDoItem from "./ToDoItem"
+
 const ToDoList = ( { todos } ) => {
   return (
     <ul className="list">
         {todos.length === 0 && "No Todos"} {/* to short circuit (stop code from executing the rest of the code) add conditional for when there are no to dos */}
         {todos.map(todo => {
           return (
-            <li key={todo.id}>
-              <label>
-                <input type="checkbox" checked={todo.completed} 
-                // onChange={e => toggleTodo(todo.id, e.target.checked)}
-                />
-                {todo.title}
-              </label>
-              <button 
-                // onClick={()=>deleteTodo(todo.id)} 
-                className="btn btn-danger"
-              >Delete</button>
-            </li>
+            // <li key={todo.id}>
+            //   <label>
+            //     <input type="checkbox" checked={todo.completed} 
+            //     // onChange={e => toggleTodo(todo.id, e.target.checked)}
+            //     />
+            //     {todo.title}
+            //   </label>
+            //   <button 
+            //     // onClick={()=>deleteTodo(todo.id)} 
+            //     className="btn btn-danger"
+            //   >Delete</button>
+            // </li>
+            <ToDoItem 
+                key={todo.id} 
+                completed={todo.completed} 
+                id={todo.id} 
+                title={todo.title}
+            />
           )
         })}
     </ul>
