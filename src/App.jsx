408c48e6584187b49dd79c8336e8656fde332b4d
juligarc91 <julissagarcia@pursuit.org>
2024-01-11 22:08:53 -0500

    Added local storage and useEffect correctly however there was a parse error

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,15 @@ import ToDoList from "./components/ToDoList";
 
 const App = () => {
    // text input
-  const [todos, setTodos] = useState([]); // list will change everytime something is added (form is submitted)
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+    return JSON.parse(localValue)
+  });  // list will change everytime something is added (form is submitted). callback inside useState fx will get info from localstorage. UseEffect will use this to make data persist
 
-  useEffect(()=>{});
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos]); // useEffect runs every time todos changes (indicated by second argument which is an array with todos current list)
   
   function addToDo (title) { // after moving everything to the new component it doesn't work like it used to because it's not using the todos useState so make new callback fx that uses both form useState called newItem and and todos useState. Put callback in the NewToDoForm component where it is needed
     setTodos(currentTodos => {
